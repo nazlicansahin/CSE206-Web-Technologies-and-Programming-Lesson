26a41c11c24e01d95616b6a8b12d1f47b15fb0ad
nzlcnshn287 <76625343+nazlicansahin@users.noreply.github.com>
2023-03-13 20:23:39 +0300

    menu button placed on all pages

--- own-website/script.js
+++ own-website/script.js
@@ -1,20 +1,19 @@
+
 let width= true;
 
 function likebutton(){
   const thanks = document.getElementById("thanks")
   const button = document.getElementById("like");
-
   if (width==true){
+    width=false;
   button.style.backgroundColor="#ead1ab";
   thanks.style.display="inline";
-  width=false;
   }
   else{
+    width=true;
     button.style.backgroundColor="#272727";
     thanks.style.display="none";
-
-    width=true;
-    
+ 
   }
 
 }
